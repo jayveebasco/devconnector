4e1357dd23116d408c4ff841b9d554bb858920c4
jayveebasco <jayveebasco.jb@gmail.com>
2020-05-05 13:33:01 +0800

    User and Profile Routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://basco:basco20@devconnector-iq06e.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://basco:basco20@devconnector-iq06e.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
 }
